a1abd0f5eb410d6970ffe5ce0e78206993be533a
EnockYator <ekyator02@gmail.com>
2024-06-21 12:45:00 +0300

    updated assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  //configuring global css
+  css: [
+    'assets/styles/main.css'
+  ]
 })
