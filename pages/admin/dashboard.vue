a6b96b004238d2200cdf27506996e34755927169
EnockYator <ekyator02@gmail.com>
2024-06-24 14:50:48 +0300

    updated admin files

---
+++ pages/admin/dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="admin-dashboard">
+        <Header />
+        <h2>Admin Dashboard</h2>
+        <Footer />
+    </div>
+  
+</template>
+
+<script>
+import Header from '/components/common/Header.vue';
+import Footer from '/components/common/Footer.vue';
+
+export default {
+    name: "dashboard",
+    components: {
+        Header,
+        Footer
+    }
+
+}
+</script>
+
+<style>
+.admin-dashboard{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+</style>
---
+++ pages/admin/manage-users.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="users-list">
+        <Header />
+        <h2>Manage Users</h2>
+        <UserCard v-for="systemUser in systemUsers" :key="systemUser.id" :user="systemUser"/>    
+        <Footer />
+    </div>
+  
+</template>
+
+<script>
+import Header from '/components/common/Header.vue';
+import Footer from '/components/common/Footer.vue';
+import UserCard from '/components/common/UserCard.vue';
+
+export default {
+    name: "manage-users",
+    components: {
+        Header,
+        Footer,
+        UserCard
+    },
+    data() {
+        return {
+            systemUsers: [
+                {id: '1', name: 'Enock', email: 'enock@gmail.com', avatar: '/assets/images/auditorium.jpg', role: 'Applicant'},
+                {id: '2', name: 'Yator', email: 'yator@gmail.com', avatar: '/assets/images/kvda-logo-image.png', role: 'hr'}
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+.users-list{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+</style>
