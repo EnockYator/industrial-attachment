e0a6d3e785a12c80e87bd3eeb10b26579bdcacfd
EnockYator <ekyator02@gmail.com>
2024-06-24 14:51:55 +0300

    updated applicant files

---
+++ pages/applicant/application-status.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="application-status">
+        <Header />
+        <h2>Application Status</h2>
+        <Footer />
+    </div>
+  
+</template>
+
+<script>
+import Header from '/components/common/Header.vue';
+import Footer from '/components/common/Footer.vue';
+
+export default {
+    name: "application-status",
+    components: {
+        Header,
+        Footer
+    }
+
+}
+</script>
+
+<style>
+.application-status{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+</style>
---
+++ pages/applicant/apply.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="apply">
+        <Header />
+        <h2>Apply</h2>
+        <Footer />
+    </div>
+  
+</template>
+
+<script>
+import Header from '/components/common/Header.vue';
+import Footer from '/components/common/Footer.vue';
+
+export default {
+    name: "apply",
+    components: {
+        Header,
+        Footer
+    }
+
+}
+</script>
+
+<style>
+.apply{
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+</style>
